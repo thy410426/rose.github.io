6e7393f86c8bb50106260d1bf6dadf637195c038
thy <136031858@qq.com>
2019-08-07 02:40:26 +0800

    ROSE

---
+++ js/index.js
@@ -0,0 +1,79 @@
+var svg = document.getElementById('div');
+
+var animation0 = document.getElementById('animate0');
+animation0.beginElement();
+
+var animation1 = document.getElementById('animate1');
+animation1.beginElement();
+
+var animation2 = document.getElementById('animate2');
+animation2.beginElement();
+
+var animation3 = document.getElementById('animate3');
+animation3.beginElement();
+
+var animation4 = document.getElementById('animate4');
+animation4.beginElement();
+
+var animation5 = document.getElementById('animate5');
+animation5.beginElement();
+
+var animation6 = document.getElementById('animate6');
+animation6.beginElement();
+
+var animation7 = document.getElementById('animate7');
+animation7.beginElement();
+
+var animation8 = document.getElementById('animate8');
+animation8.beginElement();
+
+var animation9 = document.getElementById('animate9');
+animation9.beginElement();
+
+var animation10 = document.getElementById('animate10');
+animation10.beginElement();
+
+var animation11 = document.getElementById('animate11');
+animation11.beginElement();
+
+var animation12 = document.getElementById('animate12');
+animation12.beginElement();
+
+var animation13 = document.getElementById('animate13');
+animation13.beginElement();
+
+var animation14 = document.getElementById('animate14');
+animation14.beginElement();
+
+
+// var svg = document.getElementById('div');
+// var animation0 = document.getElementById('animate0');
+// svg.addEventListener('mouseenter', function(){ animation0.beginElement(); });
+// var animation1 = document.getElementById('animate1');
+// svg.addEventListener('mouseenter', function(){ animation1.beginElement(); });
+// var animation2 = document.getElementById('animate2');
+// svg.addEventListener('mouseenter', function(){ animation2.beginElement(); });
+// var animation3 = document.getElementById('animate3');
+// svg.addEventListener('mouseenter', function(){ animation3.beginElement(); });
+// var animation4 = document.getElementById('animate4');
+// svg.addEventListener('mouseenter', function(){ animation4.beginElement(); });
+// var animation5 = document.getElementById('animate5');
+// svg.addEventListener('mouseenter', function(){ animation5.beginElement(); });
+// var animation6 = document.getElementById('animate6');
+// svg.addEventListener('mouseenter', function(){ animation6.beginElement(); });
+// var animation7 = document.getElementById('animate7');
+// svg.addEventListener('mouseenter', function(){ animation7.beginElement(); });
+// var animation8 = document.getElementById('animate8');
+// svg.addEventListener('mouseenter', function(){ animation8.beginElement(); });
+// var animation9 = document.getElementById('animate9');
+// svg.addEventListener('mouseenter', function(){ animation9.beginElement(); });
+// var animation10 = document.getElementById('animate10');
+// svg.addEventListener('mouseenter', function(){ animation10.beginElement(); });
+// var animation11 = document.getElementById('animate11');
+// svg.addEventListener('mouseenter', function(){ animation11.beginElement(); });
+// var animation12 = document.getElementById('animate12');
+// svg.addEventListener('mouseenter', function(){ animation12.beginElement(); });
+// var animation13 = document.getElementById('animate13');
+// svg.addEventListener('mouseenter', function(){ animation13.beginElement(); });
+// var animation14 = document.getElementById('animate14');
+// svg.addEventListener('mouseenter', function(){ animation14.beginElement(); });
